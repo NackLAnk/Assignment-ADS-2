2d745dca1c0e4e8dd81a3e85b7f9193296e99e8c
Alisher <www.at.7777@gmail.com>
2023-04-19 22:50:07 +0600

    V8

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MyArrayList<T> implements MyList<T> {
     // instance variables
     private Object[] arr; // array to hold the elements
@@ -49,35 +51,61 @@ public class MyArrayList<T> implements MyList<T> {
         }
         return false;
     }
-
+    // Returns the element at the specified position in the list
     public T get(int index) {
-        if (index < 0 || index >= size) { // if the given index is within the valid range of indices for the list. If the index is invalid,
-            throw new IndexOutOfBoundsException(); // throws an IndexOutOfBoundsException with an appropriate error message.
-        };
-        return (T) arr[index];
+        checkIndex(index); // Check that the index is valid
+        return (T) arr[index]; // Cast the element to the appropriate type and return it
+    }
+    // Returns the index of the first occurrence of the specified element in the list, or -1 if the element is not found
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) { // The method checks if the given index is within the valid range of indices for the list. If the index is invalid,
+            throw new IndexOutOfBoundsException(); // the method throws an IndexOutOfBoundsException with an appropriate error message.
+        }
     }
 
     public int indexOf(T e) {
-        return 0;
+        for (int i = 0; i < size; i++) { // Iterate over all elements in the list
+            if (arr[i].equals(e)) { // If the current element is equal to the specified element, return its index
+                return i;
+            }
+        }
+        return -1;
     }
-
+    // get the index of the last occurrence of an element in the list
     public int lastIndexOf(T e) {
-        return 0;
+        for (int i = size - 1; i >= 0; i--) {
+            if (arr[i].equals(e)) { // use equals() to compare the elements
+                return i;
+            }
+        }
+        return -1;
     }
-
+    // remove the first occurrence of an element from the list
     public boolean remove(T e) {
+        for (int i = 0; i < size; i++) {
+            if (arr[i].equals(e)) { // use equals() to compare the elements
+                remove(i); // remove the element at the specified index
+                return true;
+            }
+        }
         return false;
     }
 
     public T remove(int index) {
-        return null;
+        checkIndex(index);  // check if the index is valid
+        T removed = (T) arr[index]; // cast the element to the generic type and store it for later return
+        for (int i = index; i < size - 1; i++) { // shift elements to remove
+            arr[i] = arr[i + 1];
+        }
+        size--;
+        return removed;
     }
-
+    // method returns the number of elements
     public int size() {
-        return 0;
+        return size; //  It simply returns the value of the size instance variable.
     }
-
+    // method sorts the elements of the MyArrayList instance in ascending order.
     public void sort() {
-
+        Arrays.sort(arr, 0, size); //  sorts the elements of the MyArrayList instance in ascending order.
     }
 }
