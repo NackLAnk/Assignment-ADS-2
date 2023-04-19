d132e00a64940df34feb82a70cf66b6d3b18684a
Alisher <www.at.7777@gmail.com>
2023-04-19 23:10:57 +0600

    created MyLinkedList class and added all methods

---
+++ src/MyLinkedList.java
@@ -0,0 +1,50 @@
+public class MyLinkedList<E> implements MyLinkList<E>{
+
+    public void add(E e) {
+
+    }
+
+    public void add(int index, E e) {
+
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void clear() {
+
+    }
+
+    public boolean contains(E e) {
+        return false;
+    }
+
+    public E get(int index) {
+        return null;
+    }
+
+    public int indexOf(E e) {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int lastIndexOf(E e) {
+        return 0;
+    }
+
+    public boolean remove(E e) {
+        return false;
+    }
+
+    public E remove(int index) {
+        return null;
+    }
+
+    public Object set(int index, E e) {
+        return null;
+    }
+}
