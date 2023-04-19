4cd92d943b2ae3d3af2003cbce2c97e95d2fba4d
Alisher <www.at.7777@gmail.com>
2023-04-19 23:06:57 +0600

    MyLinkList V4

--- src/MyLinkList.java
+++ src/MyLinkList.java
@@ -15,15 +15,28 @@ public interface MyLinkList<E> {
 
     // Return the element from this list at the specified index
     E get(int index);
-    /** Return the index of the first matching element in this list.
-     *  Return -1 if no match. */
-    public int indexOf(E e);
+    // Return the index of the first matching element in this list.
+    //Return -1 if no match. */
+    int indexOf(E e);
 
     // Return true if this list doesn't contain any elements
-    public boolean isEmpty();
+    boolean isEmpty();
 
-    /** Return the index of the last matching element in this list
-     * Return -1 if no match. */
-    public int lastIndexOf(E e);
+    // Return the index of the last matching element in this list
+    // Return -1 if no match.
+    int lastIndexOf(E e);
 
+    // Remove the first occurrence of the element e from this list.
+    //  Shift any subsequent elements to the left.
+    //  Return true if the element is removed.
+    boolean remove(E e);
+
+    // Remove the element at the specified postion in this list.
+    //  Shift any subsequent element to the left
+    //  Return the element that was removed from the list. */
+    E remove(int index);
+
+    // Replace the element at the specified position in this list
+    // with the specified element and return the old element.
+    Object set(int index, E e);
 }
