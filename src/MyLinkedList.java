42943be9b2ed6611f8b3a199a2a0dceba7241864
Alisher <www.at.7777@gmail.com>
2023-04-19 23:31:50 +0600

    Created methods without doc

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,11 +1,69 @@
 public class MyLinkedList<E> implements MyLinkList<E>{
+    private Node<E> head, tail;
+    private int size;
+
+    private static class Node<E> {
+        private E element;
+        private Node<E> next;
+
+        public Node(E element, Node<E> next) {
+            this.element = element;
+            this.next = next;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+    }
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
 
     public void add(E e) {
-
+        Node<E> newNode = new Node<>(e, null);
+        if (isEmpty()) {
+            head = newNode;
+        } else {
+            tail.setNext(newNode);
+        }
+        tail = newNode;
+        size++;
     }
 
     public void add(int index, E e) {
-
+        checkIndexForAdd(index);
+        if (index == size) {
+            add(e);
+            return;
+        }
+        Node<E> newNode = new Node<>(e, null);
+        if (index == 0) {
+            newNode.setNext(head);
+            head = newNode;
+        } else {
+            Node<E> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.getNext();
+            }
+            newNode.setNext(current.getNext());
+            current.setNext(newNode);
+        }
+        size++;
+    }
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
     }
 
     public int size() {
